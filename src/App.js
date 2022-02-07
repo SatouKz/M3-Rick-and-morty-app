b9fdeaf3ece67bd0e386c00cf18e3777651e4185
SatouKz <alexander.ajalla404@gmail.com>
2022-02-07 17:05:08 -0300

    unstyled selector

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [listCharacters, setListCharacter] = useState();
+  // console.log(listCharacters);
+  const [selectedCharacter, setSelectedCharacter] = useState();
+  // Fetch promises
+  const initialUrl = "https://rickandmortyapi.com/api/character";
+  const fetchCharacter = (url) => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        const personajes = data.results;
+        setListCharacter(personajes);
+      })
+      .catch((err) => console.log(err));
+  };
+  //
+  useEffect(() => {
+    fetchCharacter(initialUrl);
+  }, []);
+  // Submit & filter
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const characterId = e.target.value;
+    const selectedCharacter = listCharacters.filter(
+      (character) => character.id === characterId
+    )[0];
+    setSelectedCharacter(selectedCharacter);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Rick and Morty App</h1>
+      <select onChange={(e) => submitHandler(e)} name="select" id="select">
+        <option value="">-- Select a Character --</option>
+        {listCharacters &&
+          listCharacters.map((personaje) => (
+            <option key={personaje.id} value={personaje.id}>
+              {personaje.name}
+            </option>
+          ))}
+      </select>
+      {selectedCharacter && (
+        <div className="card">
+          <img src={selectedCharacter.image} alt="Imagen" />
+          <div className="info">
+            <h3>{selectedCharacter.name}</h3>
+            <p>
+              Species: <strong>{selectedCharacter.species} </strong>
+            </p>
+            <p>
+              Status: <strong>{selectedCharacter.status}</strong>
+            </p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
